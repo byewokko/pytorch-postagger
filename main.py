c7a7574139721d3bc42b461d682ded10288267b9
Byewokko <byewokko@seznam.cz>
2019-03-07 12:48:51 +0100

    minor edits

--- main.py
+++ main.py
@@ -35,12 +35,18 @@ hparams = {
     "dropout_rate": 0.5,
 
     # number of hidden layers
+    # if you increase the number of hidden layers
+    # you can add dropout in between them
     "rnn_layer_number": 1,
 
     # length of input sentences
     # (shorter sentences are padded to match this length)
     # (do not change it)
-    "input_len": 50
+    "input_len": 50,
+
+    "optimizer": optim.Adam,
+
+    "loss_function": nn.NLLLoss(),
 }
 
 '''
@@ -80,10 +86,8 @@ class Tagger(nn.Module):
                             hidden_size=self.rnn_layer_size,
                             num_layers=self.rnn_layer_number,
                             batch_first=True,
-                            bidirectional=True,
-                            dropout=0.5)
+                            bidirectional=True)
         self.hidden = self.init_hidden()
-        # DROPOUT PLUGS IN HERE
         self.dropout = nn.Dropout(0.5)
         self.dense = nn.Linear(2 * self.rnn_layer_size, self.tagset_size)
         self.softmax = nn.LogSoftmax(dim=1)
@@ -101,15 +105,20 @@ class Tagger(nn.Module):
         return hidden, cell
 
     def forward(self, X, lengths):
-        self.hidden = self.init_hidden()
-        batch_size, sent_len = X.size()
+
         X = self.word_emb(X)
         # X = torch.nn.utils.rnn.pack_padded_sequence(X, lengths, batch_first=True)
         X, self.hidden = self.lstm(X, self.hidden)
         # X, _ = torch.nn.utils.rnn.pad_packed_sequence(X, batch_first=True)
-        X = self.dropout(X)
+
+        # here we reshape the output of the BiLSTM
         X = X.contiguous()
         X = X.view(-1, X.shape[2])
+
+
+        X = self.dropout(X)
+
+
         X = self.dense(X)
         X = self.softmax(X)
         X = X.view(-1, self.tagset_size)
@@ -117,31 +126,33 @@ class Tagger(nn.Module):
         return Y_h
 
 
-def train_model(model, train_data, dev_data, n_epochs, loss_function, optimizer, batch_size, **kwargs):
+def train_model(model, train_data, dev_data, number_of_epochs, loss_function, optimizer, learning_rate, batch_size, **kwargs):
 
-    trainloader = torch.utils.data.DataLoader(train_data,
+    optimizer = optimizer(model.parameters(), lr=learning_rate)
+
+    train_loader = torch.utils.data.DataLoader(train_data,
                                               batch_size=batch_size,
                                               shuffle=True,
                                               num_workers=8)
-    devloader = torch.utils.data.DataLoader(dev_data,
+    dev_loader = torch.utils.data.DataLoader(dev_data,
                                             batch_size=batch_size,
                                             shuffle=False,
                                             num_workers=8)
 
-    train_batches = len(trainloader)
-    dev_batches = len(devloader)
+    train_batches = len(train_loader)
+    dev_batches = len(dev_loader)
 
 
-    for epoch in range(n_epochs):
+    for epoch in range(number_of_epochs):
 
         print("epoch", epoch+1)
 
-        for batch_n, (X, Y, sent_len) in enumerate(trainloader):
+        for batch_n, (X, Y, sent_len) in enumerate(train_loader):
             print("training   |{}|".format(util.loadbar(batch_n/(train_batches-1))), end="\r")
             sys.stdout.flush()
 
-            if len(sent_len) < model.batch_size:
-                continue
+            # if len(sent_len) < model.batch_size:
+            #     continue
             max_sent = max(sent_len)
 
             model.zero_grad()
@@ -156,16 +167,16 @@ def train_model(model, train_data, dev_data, n_epochs, loss_function, optimizer,
 
         print()
 
-        for batch_n, (X, Y, sent_len) in enumerate(devloader):
+        for batch_n, (X, Y, sent_len) in enumerate(dev_loader):
             print("validation |{}|".format(util.loadbar(batch_n /(dev_batches-1))), end="\r")
             sys.stdout.flush()
 
-            if len(sent_len) < model.batch_size:
-                continue
+            # if len(sent_len) < model.batch_size:
+            #     continue
             max_sent = max(sent_len)
 
             model.zero_grad()
-            model.hidden = model.init_hidden()
+            model.hidden = model.init_hidden(batch_size=len(sent_len))
 
             Y_h = model(X, sent_len)
 
@@ -177,23 +188,20 @@ def train_model(model, train_data, dev_data, n_epochs, loss_function, optimizer,
         print("train loss", train_loss.item(), "val loss", dev_loss.item())
 
 
-def test_model(model, x, y, loss_function):
-    """
+def test_model(model, test_data, loss_function, batch_size):
 
-    :param model:
-    :param x:
-    :param y:
-    :param loss_function:
-    :return:
-    """
-    with torch.no_grad():
-        tag_scores = model(x, y)
+
+    test_loader = torch.utils.data.DataLoader(test_data,
+                                            batch_size=batch_size,
+                                            shuffle=False,
+                                            num_workers=8)
+    raise NotImplementedError
+
     return loss, accuracy
 
 #######################################
 
 def main():
-    # TODO: make dataprep more readable
     """
     " Data preparation
     """
@@ -208,28 +216,12 @@ def main():
     print("tags loaded")
 
     # Read in data and create tensors
-    # _x is the input tokens and _y is the labels
-    """train_x, train_y, train_y_list = util.prepare_data(train_file, word2i, tag2i, hparams["input_len"])
-    dev_x, dev_y, dev_y_list = util.prepare_data(dev_file, word2i, tag2i, hparams["input_len"])
-    test_x, test_y, test_y_list = util.prepare_data(test_file, word2i, tag2i, hparams["input_len"])"""
     train_data = util.prepare_data(train_file, word2i, tag2i, hparams["input_len"])
     dev_data = util.prepare_data(dev_file, word2i, tag2i, hparams["input_len"])
     test_data = util.prepare_data(test_file, word2i, tag2i, hparams["input_len"])
     print("data loaded")
 
-    tagset_size = len(tag2i)  # Numbers of tags
-
-    #print(train_x.shape, train_y.shape, file=sys.stderr)
-    #print(embeddings.shape, file=sys.stderr)
-
-    """
-    a = nn.Embedding.from_pretrained(embeddings)
-    b = nn.LSTM(50, 100)
-    c, d = b(a(train_x[:10]))
-    print(c.shape)
-    print(d)
-
-    quit()"""
+    tagset_size = len(tag2i)
 
     model = Tagger(embedding=embeddings,
                    tagset_size=tagset_size,
@@ -238,13 +230,9 @@ def main():
     train_model(model,
                 train_data=train_data,
                 dev_data=dev_data,
-                n_epochs=20,
-                loss_function=nn.NLLLoss(),
-                optimizer=optim.Adam(model.parameters(), lr=0.01),
                 **hparams)
 
-    test_model(model,
-               (test_x, test_y))
+    #test_model(model, test_data=test_data)
 
 if __name__ == "__main__":
     main()
